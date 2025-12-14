934533f91eaaa5c1cd087103d4f6f757e8ee934c
Bungarasikhahhaya <rasikhahhayaa@gmail.com>
2025-12-14 20:31:38 +0700

    fix github actions nextjs pages

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,16 @@
-// next.config.ts
 import type { NextConfig } from 'next';
 
-const repoName = 'DigiArchive'; 
+const repoName = 'DigiArchive';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-
-  // WAJIB untuk GitHub Pages
   output: 'export',
 
-  // WAJIB agar Image tidak error
   images: {
     unoptimized: true,
   },
 
-  // WAJIB jika repo BUKAN username.github.io
   basePath: `/${repoName}`,
   assetPrefix: `/${repoName}/`,
-
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        path: false,
-        os: false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
